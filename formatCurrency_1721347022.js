8870a1f1024b2fa092adbd018ba3aa2514c8585f
adil-sadout <adelsadout@hotmail.com>
2023-03-04 19:00:00 +0100

    Commit on 2023-03-04T19:00:00+01:00

---
+++ formatCurrency_1721347022.js
@@ -0,0 +1,9 @@
+// Function: formatCurrency - Fri Jul 19 00:57:02     2024
+function formatCurrency() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("formatCurrency result:", result);
+}
+formatCurrency();
