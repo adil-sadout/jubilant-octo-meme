3e056c479a80c104c26c126486179dc96e91ae01
adil-sadout <adelsadout@hotmail.com>
2023-05-02 07:00:00 +0100

    Commit on 2023-05-02T07:00:00+01:00

---
+++ filterData_1721347137.js
@@ -0,0 +1,9 @@
+// Function: filterData - Fri Jul 19 00:58:57     2024
+function filterData() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("filterData result:", result);
+}
+filterData();
