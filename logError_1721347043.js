7dd1626c5ba6e461799ef4a199d69346f11663bc
adil-sadout <adelsadout@hotmail.com>
2023-07-22 19:00:00 +0100

    Commit on 2023-07-22T19:00:00+01:00

---
+++ logError_1721347043.js
@@ -0,0 +1,9 @@
+// Function: logError - Fri Jul 19 00:57:23     2024
+function logError() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("logError result:", result);
+}
+logError();
