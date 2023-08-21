df675229f2eac02dd31a053c17aeb1b0dce4dd38
adil-sadout <adelsadout@hotmail.com>
2023-08-21 19:00:00 +0100

    Commit on 2023-08-21T19:00:00+01:00

---
+++ uploadImage_1721347046.js
@@ -0,0 +1,9 @@
+// Function: uploadImage - Fri Jul 19 00:57:26     2024
+function uploadImage() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("uploadImage result:", result);
+}
+uploadImage();
