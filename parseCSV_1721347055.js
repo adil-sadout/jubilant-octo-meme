b7dd24f0571a050f5fa16e18f3138a294e716e60
adil-sadout <adelsadout@hotmail.com>
2023-11-18 19:00:00 +0100

    Commit on 2023-11-18T19:00:00+01:00

---
+++ parseCSV_1721347055.js
@@ -0,0 +1,9 @@
+// Function: parseCSV - Fri Jul 19 00:57:35     2024
+function parseCSV() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("parseCSV result:", result);
+}
+parseCSV();
