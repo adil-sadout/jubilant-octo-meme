5a483adc9b4216a0cd2b4ca472909ba47f9bb2d8
adil-sadout <adelsadout@hotmail.com>
2023-12-11 19:00:00 +0100

    Commit on 2023-12-11T19:00:00+01:00

---
+++ sanitizeInput_1721347059.js
@@ -0,0 +1,9 @@
+// Function: sanitizeInput - Fri Jul 19 00:57:39     2024
+function sanitizeInput() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("sanitizeInput result:", result);
+}
+sanitizeInput();
