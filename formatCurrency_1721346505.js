927ed20d741800bf8fc772d5748af8fd2ec180f5
adil-sadout <adelsadout@hotmail.com>
2024-01-17 00:00:00 +0100

    Commit on 2024-01-17T00:00:00+01:00

---
+++ formatCurrency_1721346505.js
@@ -0,0 +1,9 @@
+// Function: formatCurrency - Fri Jul 19 00:48:25     2024
+function formatCurrency() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("formatCurrency result:", result);
+}
+formatCurrency();
