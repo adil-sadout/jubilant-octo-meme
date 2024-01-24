5342cc74f05265f62472cc3ca8f6871977584b69
adil-sadout <adelsadout@hotmail.com>
2024-01-25 00:00:00 +0100

    Commit on 2024-01-25T00:00:00+01:00

---
+++ logActivity_1721346508.js
@@ -0,0 +1,9 @@
+// Function: logActivity - Fri Jul 19 00:48:29     2024
+function logActivity() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("logActivity result:", result);
+}
+logActivity();
