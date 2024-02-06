d30389b23265024a6615b50e43df0d6a2e75fef6
adil-sadout <adelsadout@hotmail.com>
2024-02-07 00:00:00 +0100

    Commit on 2024-02-07T00:00:00+01:00

---
+++ sortArray_1721346512.js
@@ -0,0 +1,9 @@
+// Function: sortArray - Fri Jul 19 00:48:32     2024
+function sortArray() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("sortArray result:", result);
+}
+sortArray();
