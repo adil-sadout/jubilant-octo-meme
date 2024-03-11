85491894b42cde1aaefbc1b42262a58993ca8606
adil-sadout <adelsadout@hotmail.com>
2024-03-11 22:00:00 +0100

    Commit on 2024-03-11T22:00:00+01:00

---
+++ calculateAverage_1721346666.js
@@ -0,0 +1,9 @@
+// Function: calculateAverage - Fri Jul 19 00:51:06     2024
+function calculateAverage() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("calculateAverage result:", result);
+}
+calculateAverage();
