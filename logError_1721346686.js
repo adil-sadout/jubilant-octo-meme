deb6da04a53f5e38069fc34249f9d9c7c9b0647d
adil-sadout <adelsadout@hotmail.com>
2024-06-05 22:00:00 +0100

    Commit on 2024-06-05T22:00:00+01:00

---
+++ logError_1721346686.js
@@ -0,0 +1,9 @@
+// Function: logError - Fri Jul 19 00:51:26     2024
+function logError() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("logError result:", result);
+}
+logError();
