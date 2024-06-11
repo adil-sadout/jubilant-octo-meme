5a3c2a8c9ed180186c59b2316883412394ce7120
adil-sadout <adelsadout@hotmail.com>
2024-06-11 22:00:00 +0100

    Commit on 2024-06-11T22:00:00+01:00

---
+++ formatDate_1721346688.js
@@ -0,0 +1,9 @@
+// Function: formatDate - Fri Jul 19 00:51:28     2024
+function formatDate() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("formatDate result:", result);
+}
+formatDate();
