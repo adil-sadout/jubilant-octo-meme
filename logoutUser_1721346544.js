8a2fa80086de737a2b3c1e26855eb0f336771e0b
adil-sadout <adelsadout@hotmail.com>
2024-06-20 00:00:00 +0100

    Commit on 2024-06-20T00:00:00+01:00

---
+++ logoutUser_1721346544.js
@@ -0,0 +1,9 @@
+// Function: logoutUser - Fri Jul 19 00:49:04     2024
+function logoutUser() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        result += i;
+    }
+    console.log("logoutUser result:", result);
+}
+logoutUser();
